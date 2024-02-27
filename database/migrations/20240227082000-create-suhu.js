e36bcd76630ba964532202924afb2a6950d7d549
alfarizymourinho <alfarizymourinho@gmail.com>
2024-02-27 15:44:49 +0700

    Backend languageloop

---
+++ database/migrations/20240227082000-create-suhu.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Suhus', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      tanggal: {
+        type: Sequelize.DATE
+      },
+      judul: {
+        type: Sequelize.STRING
+      },
+      isi: {
+        type: Sequelize.STRING
+      },
+      materi: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Suhus');
+  }
+};
